3d0fe6818b4d1f25a2ad89e36d6b52825958f3ea
Anglindw <dwanglin@gmail.com>
2022-12-13 19:19:02 -0600

    hhhh

---
+++ main.js
@@ -0,0 +1,11 @@
+const day = new Date().getDay()
+console.log(day)
+const literal_date = new Date().toDateString()
+console.log(literal_date)
+
+switch(day){
+    case 1:
+        console.log('it monday')
+        break;
+    c
+}
